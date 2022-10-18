9924aac66d5fa65f03aeea55b75244b7a5341908
arif1408 <maarifbatubara@gmail.com>
2022-10-18 18:44:00 +0700

    dashboard bijakasana
    dashboard

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        // return 'this is dashboard';
+        return view('admin.dashboard')
+    }
+}
